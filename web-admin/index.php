a91a331dde52c058fc802e8a6bc84af830803984
matthew <matthew>
2005-07-19 09:44:31 +0000

    Start of webadmin page.

---
+++ web-admin/index.php
@@ -0,0 +1,33 @@
+<?
+/*
+ * index.php:
+ * Admin pages for PledgeBank.
+ * 
+ * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+ * Email: francis@mysociety.org. WWW: http://www.mysociety.org
+ *
+ * $Id: index.php,v 1.1 2005-07-19 09:44:31 matthew Exp $
+ * 
+ */
+
+require_once "../conf/general";
+require_once "../phplib/admin-ycml.php";
+require_once "../../phplib/template.php";
+require_once "../../phplib/admin-phpinfo.php";
+require_once "../../phplib/admin-serverinfo.php";
+require_once "../../phplib/admin-configinfo.php";
+require_once "../../phplib/admin.php";
+
+$pages = array(
+    new ADMIN_PAGE_YCML_LATEST,
+    new ADMIN_PAGE_YCML_MAIN,
+    new ADMIN_PAGE_YCML_ABUSEREPORTS,
+    null, // space separator on menu
+    new ADMIN_PAGE_SERVERINFO,
+    new ADMIN_PAGE_CONFIGINFO,
+    new ADMIN_PAGE_PHPINFO,
+);
+
+admin_page_display(str_replace("http://", "", OPTION_BASE_URL . "/"), $pages);
+
+?>
