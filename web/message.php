5394e5ae3f61a944401abaf48511f557f0d15f9a
sandpit <sandpit>
2005-11-07 16:11:02 +0000

    Bug fix; warn about requirement to confirm.

--- web/message.php
+++ web/message.php
@@ -6,12 +6,15 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: message.php,v 1.2 2005-11-04 15:20:14 chris Exp $
+ * $Id: message.php,v 1.3 2005-11-07 16:11:02 sandpit Exp $
  * 
  */
 
+require_once '../phplib/ycml.php';
+require_once '../../phplib/importparams.php';
+
 importparams(
-        array('t',              '/.+$/',            '', null)
+        array('t',              '/.+$/',            '', null),
         array('post',           '/.+$/',            '', false)
     );
 
@@ -32,7 +35,7 @@ if ($q_post) {
 } else {
     page_header("Confirm a message");
     list($subject, $content)
-        = db_getRow('select subject, content from message where id = ?', $r['id']);
+        = db_getRow_list('select subject, content from message where id = ?', $r['id']);
     ?>
 <p>Here is the text of the message, as it will appear to subscribers. Please
 check through this and, if all is well, click the "Yes, send it" button at the
@@ -47,6 +50,7 @@ bottom of the page:</p>
     print $content;
     ?>
 </p></blockquote>
+</div>
 <form method="POST">
 <p>If this message looks OK, then please click
 <input type="hidden" name="t" value="<?=htmlspecialchars($q_t)?>">
@@ -55,6 +59,7 @@ and we'll send it to your subscibers immediately. Otherwise, please get in
 touch with us at
 <a href="mailto:<?=htmlspecialchars(OPTION_CONTACT_EMAIL)?>"><?=htmlspecialchars(OPTION_CONTACT_EMAIL)?></a>
 and we'll get back to you as soon as possible.</p>
+<p><strong>You must confirm your message for it to be sent to constituents. If you do not confirm the message it will not be sent.</strong> Please mail us at <a href="mailto:team@hearfromyourmp.com">team@hearfromyourmp.com</a> if you have any questions or anything is wrong with the appearance of the message.</p>
 </form>
     <?
     page_footer();
