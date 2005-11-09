7269d64f69567272e7ffd96c1f507e6834950486
sandpit <sandpit>
2005-11-09 18:37:33 +0000

    Commit!

--- web/message.php
+++ web/message.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: message.php,v 1.5 2005-11-09 18:23:56 sandpit Exp $
+ * $Id: message.php,v 1.6 2005-11-09 18:37:33 sandpit Exp $
  * 
  */
 
@@ -24,7 +24,9 @@ if (is_null($q_t)) {
 
 $r = auth_token_retrieve('message', $q_t);
 
-db_query("update message set state = 'ready' where id = ?", $r['id']);
+db_query("update message set state = 'approved' where id = ?", $r['id']);
+db_commit();
+
 page_header("Thanks - the message will now be sent");
 ?>
 <p>Thanks. We'll now send that message out to your subscribers.</p>
