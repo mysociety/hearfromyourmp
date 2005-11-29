87202772fdd9ffc2b087f7b6b4ca81765890d5c1
sandpit <sandpit>
2005-11-29 12:58:30 +0000

    Change front page text

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: index.php,v 1.16 2005-11-22 16:38:48 francis Exp $
+// $Id: index.php,v 1.17 2005-11-29 12:58:30 sandpit Exp $
 
 require_once '../phplib/ycml.php';
 require_once '../phplib/fns.php';
@@ -15,7 +15,8 @@ front_page();
 page_footer();
 
 function front_page() { ?>
-<h2 align="center">Sign up to hear from your MP about local issues,<br>and to discuss them with other constituents</h2>
+<h2 align="center">Get email from your MP
+<br>Discuss it with your MP and other local people</h2>
 <form method="post" action="/subscribe" name="frontpage_subscribe" accept-charset="utf-8">
 <div id="subscribeBox">
     <input type="hidden" name="subscribe" id="subscribe" value="1">
@@ -31,7 +32,7 @@ function front_page() { ?>
     <br><em>(for example OX1 3DR)</em>
     </div>
 </form>
-<p style="text-align: center;"><a href="/about">Tell me more</a></p>
+<p style="text-align: center;"><a href="/about"><big>Tell me more</big></a></p>
 <?
         $people = db_getOne('SELECT COUNT(DISTINCT(person_id)) FROM constituent');
         # Minus one in the next row to account for test constituency
