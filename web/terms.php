29736818bab44387e29a1876e91e079ba08f14c5
chris <chris>
2005-12-02 17:35:45 +0000

    Typos.

--- web/terms.php
+++ web/terms.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: terms.php,v 1.1 2005-10-14 17:34:49 matthew Exp $
+// $Id: terms.php,v 1.2 2005-12-02 17:35:45 chris Exp $
 
 require_once '../phplib/ycml.php';
 require_once '../phplib/fns.php';
@@ -22,14 +22,14 @@ part of hard-worn experience from local political discussion forums
 around the world, and it has been proven to encourage people to think
 and consider more before they post.
 
-<li>The purposes of each discussion is for constructive debate on the
+<li>The purpose of each discussion is for constructive debate on the
 issues set forth in each email the MP sends.  You can write to your MP
 asking for him/her to discuss certain issues, but you must stick
-roughly to the topic raised
+roughly to the topic raised.
 
 <li>The following behaviours are deemed unacceptable, and will result
 in your posts being deleted, and/or your account being removed: ad
-hominem attacks on the MP or other users, irrelevent and off-topic
+hominem attacks on the MP or other users, irrelevant and off-topic
 responses (see 2), and partisan posturing which isn't really trying to
 solve the problems being discussed. This may all seem a bit tough, but
 HearFromYourMP is a public space, and we reserve all rights to keep it
