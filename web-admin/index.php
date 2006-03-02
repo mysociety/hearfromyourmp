4709c15b85d2e5dafc1df7dfc2367db30a6b1f62
francis <francis>
2006-03-02 15:02:33 +0000

    Make signup rate graph automatically regularly

--- web-admin/index.php
+++ web-admin/index.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: index.php,v 1.7 2006-01-06 12:23:45 matthew Exp $
+ * $Id: index.php,v 1.8 2006-03-02 15:02:35 francis Exp $
  * 
  */
 
@@ -16,6 +16,7 @@ require_once "../../phplib/template.php";
 require_once "../../phplib/admin-phpinfo.php";
 require_once "../../phplib/admin-serverinfo.php";
 require_once "../../phplib/admin-configinfo.php";
+require_once "../../phplib/admin-embed.php";
 require_once "../../phplib/admin.php";
 
 $pages = array(
@@ -24,6 +25,7 @@ $pages = array(
     new ADMIN_PAGE_YCML_SUBSCRIBERS,
     new ADMIN_PAGE_YCML_MULTIPLE,
     new ADMIN_PAGE_YCML_ABUSEREPORTS,
+    new ADMIN_PAGE_EMBED('ycmlsignupgraph', 'Signup graph', OPTION_BASE_URL . '/ycml-live-signups.png'),
     null, // space separator on menu
     new ADMIN_PAGE_SERVERINFO,
     new ADMIN_PAGE_CONFIGINFO,
