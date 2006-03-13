28b37548b4173cc229844d8779152098561f7309
francis <francis>
2006-03-13 13:14:31 +0000

    Fetch web bugs

--- web/subscribe.php
+++ web/subscribe.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: subscribe.php,v 1.20 2005-12-09 14:37:32 matthew Exp $
+// $Id: subscribe.php,v 1.21 2006-03-13 13:14:32 francis Exp $
 
 require_once '../phplib/ycml.php';
 require_once '../phplib/fns.php';
@@ -16,6 +16,7 @@ require_once '../../phplib/importparams.php';
 
 $title = _('Signing up');
 page_header($title);
+$extra = '';
 if (get_http_var('subscribe')) {
     $errors = do_subscribe();
     if (is_array($errors)) {
@@ -42,10 +43,10 @@ if (get_http_var('subscribe')) {
 } else {
     constituent_subscribe_box();
 }
-page_footer();
+page_footer(array('extra' => $extra));
 
 function do_subscribe() {
-    global $q_email, $q_name, $q_postcode, $q_h_postcode;
+    global $q_email, $q_name, $q_postcode, $q_h_postcode, $extra;
     $errors = importparams(
                 array('name',      "/./", 'Please enter a name'),
                 array('email',      "importparams_validate_email"),
@@ -115,6 +116,7 @@ continue
                 $q_postcode, $_SERVER['REMOTE_ADDR']
             ));
     db_commit();
+    $extra = "subscribed=1";
     }
     $count = db_getOne("select count(*) from constituent where constituency = ?", $wmc_id);
     $nothanks = db_getRow('SELECT status,website,gender FROM mp_nothanks WHERE constituency = ?', $wmc_id);
