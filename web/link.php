8ca674788ca1986e45dda52c5b7ef263cd3846df
matthew <matthew>
2007-12-14 16:27:40 +0000

    And colours.

--- web/link.php
+++ web/link.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: link.php,v 1.7 2007-12-14 16:22:28 matthew Exp $
+// $Id: link.php,v 1.8 2007-12-14 16:27:40 matthew Exp $
 
 require_once '../phplib/ycml.php';
 
@@ -15,13 +15,24 @@ page_footer();
 
 function link_box() {
     $example_postcode = get_example_postcode();
+    if (OPTION_AREA_ID==2326) {
+        $bgcolor = '#eeffee';
+        $font = 'Helvetica, Arial, sans-serif';
+        $border = '#02927b';
+        $link = '#000000';
+    } else {
+        $bgcolor = '#d9ecff';
+        $font = 'Georgia, serif';
+        $border = '#c06db3';
+        $link = '#20005c';
+    }
     $html = '
-<form style="text-align: left; position: relative; background-color: #d9ecff;
-        color: #000000; font-family: Georgia, serif; display:table; _width: 24em;
-        margin: 0 0 0 0; border: solid 2px #c06db3; padding: 0 0 0 0;"
+<form style="text-align: left; position: relative; background-color: ' . $bgcolor . ';
+        color: #000000; font-family: ' . $font . '; display:table; _width: 24em;
+        margin: 0 0 0 0; border: solid 2px ' . $border . '; padding: 0 0 0 0;"
         action="http://' . $_SERVER['HTTP_HOST'] . '/subscribe">
     <p style="padding: 0.5em 0.5em 0.5em 0.5em; margin: 0 0 0 0;">
-        <a href="http://' . $_SERVER['HTTP_HOST'] . '/" style="color: #20005C; 
+        <a href="http://' . $_SERVER['HTTP_HOST'] . '/" style="color: ' . $link . '; 
             text-decoration:none; font-weight:bold">
         Sign up to ' . $_SERVER['site_name'] . '</a>
         <br><strong>Name:</strong> <input type="text" name="name" id="name" size="20">
@@ -30,7 +41,7 @@ function link_box() {
         <input type="hidden" name="subscribe" id="subscribe" value="1">
         <input type="submit" class="submit" value="Sign up">
         <br><em>(for example ' . $example_postcode . ')</em>
-        <a href="http://' . $_SERVER['HTTP_HOST'] . '/about" style="color: #20005C; font-weight:bold">
+        <a href="http://' . $_SERVER['HTTP_HOST'] . '/about" style="color: ' . $link . '; font-weight:bold">
         Tell me more</a>
     </p>
 </form>
