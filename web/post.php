ca7aad32316002e4ac8d711a1c131f57b72cb049
angie <angie>
2007-12-17 18:51:26 +0000

    removing action attribute from form didn't work

--- web/post.php
+++ web/post.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: post.php,v 1.17 2007-12-17 18:48:48 angie Exp $
+// $Id: post.php,v 1.18 2007-12-17 18:51:26 angie Exp $
 
 require_once '../phplib/ycml.php';
 require_once '../phplib/constituent.php';
@@ -81,7 +81,7 @@ if ($q_post == 2) { # Post
 ?>
 </pre>
 <p>If you are happy with this, please click this button to confirm your message:</p>
-<form method="post" name="confirm_form" action="post.php" accept-charset="UTF-8">
+<form method="post" name="confirm_form" accept-charset="UTF-8">
 <input type="hidden" name="post" value="2">
 <input type="hidden" name="subject" value="<?=$q_h_subject ?>">
 <input type="hidden" name="message" value="<?=$q_h_message ?>">
@@ -133,7 +133,7 @@ page_footer();
 function post_message_form() {
     global $q_h_subject, $q_h_message;
 ?>
-<form method="post" name="message_form" action="post.php" accept-charset="UTF-8">
+<form method="post" name="message_form" accept-charset="UTF-8">
 <input type="hidden" name="post" value="1">
 <table cellpadding="3" cellspacing="0" border="0">
 <tr><th><label for="subject">Subject:</label></th>
