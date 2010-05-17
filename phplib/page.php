06c8434112bb228b71f34ea39c6f7316efd239b4
Louise Crow <louise.crow@gmail.com>
2010-05-17 17:21:00 +0100

    Updating piwik tracking js

--- phplib/page.php
+++ phplib/page.php
@@ -127,17 +127,14 @@ Powered by <a href="http://www.easynet.net/publicsector/">Easynet</a>.
 <script type="text/javascript">
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.mysociety.org/" : "http://piwik.mysociety.org/");
 document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-<!--
-piwik_action_name = '';
-piwik_idsite = 3;
-piwik_url = pkBaseURL + "piwik.php";
-piwik_log(piwik_action_name, piwik_idsite, piwik_url);
-//-->
-</script>
-<noscript><img src="http://piwik.mysociety.org/piwik.php?idsite=3" style="border:0" alt=""></noscript>
-<!-- /Piwik -->
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 3);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://piwik.mysociety.org/piwik.php?idsite=3" style="border:0" alt=""/></p></noscript>
+<!-- End Piwik Tag -->
 <?  } ?>
 </body>
 </html>
